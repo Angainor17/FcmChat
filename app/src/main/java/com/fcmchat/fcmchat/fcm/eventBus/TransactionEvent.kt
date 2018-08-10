127c334df3b0512b8af6303ca8e61dbc25550980
Voronin Igor <angainor17@gmail.com>
2018-08-10 17:44:59 +0300

    crash fix

--- app/src/main/java/com/fcmchat/fcmchat/fcm/eventBus/TransactionEvent.kt
+++ app/src/main/java/com/fcmchat/fcmchat/fcm/eventBus/TransactionEvent.kt
@@ -1,5 +1,6 @@
 package com.fcmchat.fcmchat.fcm.eventBus
 
+import com.fcmchat.fcmchat.fcm.transactions.DefaultTransaction
 import com.fcmchat.fcmchat.fcm.transactions.FcmTransaction
 import com.fcmchat.fcmchat.fcm.transactions.TransactionBuilder
 import com.google.gson.Gson
@@ -13,8 +14,7 @@ class TransactionEvent : Event() {
 
     override fun setMap(map: Map<String, String>) {
         val json = map[getKey()]
-        val transactionAbstraction = Gson().fromJson(json, FcmTransaction::class.java)!!
-
+        val transactionAbstraction = Gson().fromJson(json, DefaultTransaction::class.java)!!
 
         transaction = TransactionBuilder.transactions().first { it.type == transactionAbstraction.type }
         transaction.init(json!!)
